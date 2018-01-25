e7b7266b40e1f50987fe9dcee306f6ba3895f58a
Chiril <td1900000@gmail.com>
2018-01-25 15:58:29 +0200

    добавил кнопки
    Не понимаю почему не работает даже одна кнопка, уже 10 видео пересмотрел, не понимаю в чем ошибка

--- app/src/main/java/com/example/user/alarmclock/MainActivity.java
+++ app/src/main/java/com/example/user/alarmclock/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.user.alarmclock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageBut2:
+                Intent intent = new Intent(this, EditAlarm.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
