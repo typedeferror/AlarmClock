3fc5a7b72a3249c79dab585c19f3dc2ea151d54e
Chiril <td1900000@gmail.com>
2018-01-27 00:04:06 +0200

    Добавил Navigation View
    Работают исправно, проверил текстом, при обращении отображают его, конечно знаю что медленно работаю. Дальше,Больше,Быстрее.

--- app/src/main/java/com/example/user/alarmclock/AddAlarm.java
+++ app/src/main/java/com/example/user/alarmclock/AddAlarm.java
@@ -1,7 +1,11 @@
 package com.example.user.alarmclock;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AddAlarm extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class AddAlarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
+
+        BottomNavigationView bottomNavigationView =(BottomNavigationView) findViewById(R.id.bottoms_settings);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_repeat:
+                        Toast.makeText(AddAlarm.this, "Action Repeat Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_label:
+                        Toast.makeText(AddAlarm.this, "Action Label Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_snooze:
+                        Toast.makeText(AddAlarm.this, "Action Snooze Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
